544ce5db387e15c78b259b6f7b9f34ddd034490b
Michalis Kargakis <kargakis@protonmail.com>
2022-12-09 12:08:21 +0100

    WIP

--- src/call.ts
+++ src/call.ts
@@ -30,3 +30,32 @@ export async function all<T extends any[] = any[]>(
   }
   return callResult;
 }
+
+// WIP
+async function all3<T extends any[] = any[]>(
+  calls: ContractCall[],
+  multicallAddress: string,
+  provider: Provider,
+): Promise<T> {
+  const multicall = new Contract(multicallAddress, multicallAbi, provider);
+  const callRequests = calls.map(call => {
+    const callData = Abi.encode(call.name, call.inputs, call.params);
+    return {
+      target: call.contract.address,
+      allowFailure: true,
+      callData,
+    };
+  });
+  const response = await multicall.aggregate3(callRequests);
+  const callCount = calls.length;
+  const callResult = [] as T;
+  for (let i = 0; i < callCount; i++) {
+    const outputs = calls[i].outputs;
+    // TODO: const success = response[i][0];
+    const returnData = response[i][1];
+    const params = Abi.decode(outputs, returnData);
+    const result = outputs.length === 1 ? params[0] : params;
+    callResult.push(result);
+  }
+  return callResult;
+}
